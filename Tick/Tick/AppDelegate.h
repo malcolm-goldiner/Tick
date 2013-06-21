03489fb0b19bafbd4c46463ba27bb106e6b0d8c8
mgoldiner <mgoldiner@yahoo.com>
2013-06-20 21:28:55 -0400

    Initial Commit

---
+++ Tick/Tick/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Tick
+//
+//  Created by Malcolm Goldiner on 6/20/13.
+//  Copyright (c) 2013 Mac Help NYC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Tick/Tick/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Tick
+//
+//  Created by Malcolm Goldiner on 6/20/13.
+//  Copyright (c) 2013 Mac Help NYC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
